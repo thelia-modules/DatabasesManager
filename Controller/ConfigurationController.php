e50645261dac662bc50d0b9d8f6994567e8b5022
Jérôme Billiras <jerome.billiras+github@gmail.com>
2015-05-26 10:53:49 +0200

    Update license from GPL to LGPL

--- Controller/ConfigurationController.php
+++ Controller/ConfigurationController.php
@@ -1,25 +1,25 @@
 <?php
-/**********************************************************************************/
-/*                                                                                */
-/*    DatabasesManager - A Thelia 2 databases manager module                      */
-/*    Copyright (C) 2015 Jérôme BILLIRAS                                          */
-/*                                                                                */
-/*    This file is part of DatabasesManager.                                      */
-/*                                                                                */
-/*    DatabasesManager is free software: you can redistribute it and/or modify    */
-/*    it under the terms of the GNU General Public License as published by        */
-/*    the Free Software Foundation, either version 3 of the License, or           */
-/*    any later version.                                                          */
-/*                                                                                */
-/*    DatabasesManager is distributed in the hope that it will be useful,         */
-/*    but WITHOUT ANY WARRANTY; without even the implied warranty of              */
-/*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               */
-/*    GNU General Public License for more details.                                */
-/*                                                                                */
-/*    You should have received a copy of the GNU General Public License           */
-/*    along with this program. If not, see <http://www.gnu.org/licenses/>.        */
-/*                                                                                */
-/**********************************************************************************/
+/*************************************************************************************/
+/*                                                                                   */
+/*    DatabasesManager - A Thelia 2 databases manager module                         */
+/*    Copyright (C) 2015 Jérôme BILLIRAS                                             */
+/*                                                                                   */
+/*    This file is part of DatabasesManager.                                         */
+/*                                                                                   */
+/*    DatabasesManager is free software: you can redistribute it and/or modify       */
+/*    it under the terms of the GNU Lesser General Public License as published by    */
+/*    the Free Software Foundation, either version 3 of the License, or              */
+/*    any later version.                                                             */
+/*                                                                                   */
+/*    DatabasesManager is distributed in the hope that it will be useful,            */
+/*    but WITHOUT ANY WARRANTY; without even the implied warranty of                 */
+/*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  */
+/*    GNU Lesser General Public License for more details.                            */
+/*                                                                                   */
+/*    You should have received a copy of the GNU Lesser General Public License       */
+/*    along with this program. If not, see <http://www.gnu.org/licenses/>.           */
+/*                                                                                   */
+/*************************************************************************************/
 
 namespace DatabasesManager\Controller;
 
